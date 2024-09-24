8ae3310059cf2fe0ad3381f33454498d4ccab83c
bitcoin7661 <wotjd7661@gmail.com>
2024-09-25 00:38:14 +0900

    최종과제(연습용)

--- src/main/java/dto/Book.java
+++ src/main/java/dto/Book.java
@@ -1,22 +1,24 @@
 package dto;
 
 import java.io.Serializable;
+
 public class Book implements Serializable {
 	
 	private static final long serialVersionUID = -4274700572038677000L;
 	
-	private String bookId; //도서
+	private String bookId; //도서 ID
 	private String name; //도서명
 	private int unitPrice; //가격
-	private String author; //
-	private String description; //설
+	private String author; //저
+	private String description; //설명
 	private String publisher; //출판
-	private String category;  //분
-	private long unitsInStock; //재고 개
+	private String category;  //분류
+	private long unitsInStock; //재고 개수
 	private String releaseDate; //출판일(월/년)
 	private String condition; //신제품 구제품 리퍼브제품]
 	
 	//생성자
+	
 	public Book() { 
 		super(); // Serializable() 부모 클래스와 생성자를 호출하
 	}
@@ -27,21 +29,18 @@ public class Book implements Serializable {
 	}
 	
 	//Getter Setter 함수
-	public String getBookId() {
-		return bookId;
-	}
+	public String getBookId() { return bookId;}
+	
 	public void setBookId(String bookId) {
 		this.bookId = bookId;
 	}
-	public String getName() {
-		return name;
-	}
+	public String getName() { return name; }
+	
 	public void setName(String name) {
 		this.name = name;
 	}
-	public int getUnitPrice() {
-		return unitPrice;
-	}
+	public int getUnitPrice() { return unitPrice; }
+	
 	public void setUnitPrice(int unitPrice) {
 		this.unitPrice = unitPrice;
 	}
--- src/main/webapp/books.jsp
+++ src/main/webapp/books.jsp
@@ -1,18 +1,19 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
-    <%@ page import = "java.util.ArrayList" %>
-    <%@ page import ="dto.Book" %>
+
+<%@ page contentType="text/html; charset=utf-8" %>
+<%@ page import = "java.util.ArrayList" %>
+<%@ page import = "dto.Book" %>
     <jsp:useBean id="bookDAO" class="dao.BookRepository" scope="session"/>
-<!DOCTYPE html>
 <html>
 <head>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha/dist/css/bootstrap.min.css"
-rel= "styesheet">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/
+bootstrap.min.css" rel= "stylesheet">
 
-<meta charset="UTF-8">
 <title>도서 목록</title>
 </head>
 <body>
+<div class="container py-4">
+	<%@ include file="menu.jsp" %>
+	
 	<div class="p-5 mb-4 bg-body-tertiary rounded-3">
 		<div class="container-fluid py-5">
 			<h1 class = "display-5 fw-bold">도서목록</h1>
@@ -27,7 +28,7 @@ rel= "styesheet">
 	<div class="row align-items-md-stretch text-center">
 	
 	<% 
-		for (int i=0; i<listOfbBooks.size(); i++) {
+		for (int i=0; i< listOfBooks.size(); i++) {
 		Book book = listOfBooks.get(i);
 	%>
 	
@@ -48,8 +49,5 @@ rel= "styesheet">
 		<%@ include file="footer.jsp" %>
 		
 </div>
-		
-
-
 </body>
 </html>
